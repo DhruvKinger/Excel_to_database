9c43feba60f5a81f2f3c2b2b195e0457b8f2d946
Dhruv Kinger <cse1785@outlook.com>
2019-11-26 18:32:39 +0530

    change

--- apptesting/Controllers/HomeController.cs
+++ apptesting/Controllers/HomeController.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,9 +12,73 @@ namespace apptesting.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+       // private Entities db = new Entities();
+        DataTable dt;
+        SqlBulkCopy sqlBulk;
+
+        [HttpGet]
+        public ActionResult UploadQuestions()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Uploadquestions()
+        {
+            if (Request.Files["FileUpload1"].ContentLength > 0)
+            {
+                string extension = System.IO.Path.GetExtension(Request.Files["FileUpload1"].FileName).ToLower();
+                string connString = "";
+                string[] validFileTypes = { ".xls", ".xlsx", ".csv" };
+
+                string path1 = string.Format("{0}/{1}", Server.MapPath("~/Content/Uploads"), Request.Files["FileUpload1"].FileName);
+                if (!Directory.Exists(path1))
+                {
+                    Directory.CreateDirectory(Server.MapPath("~/Content/Uploads"));
+                }
+                if (validFileTypes.Contains(extension))
+                {
+                    if (System.IO.File.Exists(path1))
+                    { System.IO.File.Delete(path1); }
+                    Request.Files["FileUpload1"].SaveAs(path1);
+                    if (extension == ".csv")
+                    {
+                        dt = Utility.ReadAsDataTable(path1);
+                        ViewBag.Data = dt;
+                    }
+                    //Connection String to Excel Workbook  
+                    else if (extension.Trim() == ".xls")
+                    {
+                        connString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + path1 + ";Extended Properties=\"Excel 8.0;HDR=Yes;IMEX=2\"";
+                        dt = Utility.ReadAsDataTable(path1);
+                        ViewBag.Data = dt;
+                    }
+                    else if (extension.Trim() == ".xlsx")
+                    {
+                        connString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + path1 + ";Extended Properties=\"Excel 12.0;HDR=Yes;IMEX=2\"";
+                        dt = Utility.ReadAsDataTable(path1);
+                        ViewBag.Data = dt;
+                    }
+
+                }
+                else
+                {
+                    ViewBag.Error = "Please Upload Files in .xls, .xlsx or .csv format";
+
+                }
+
+
+            }
+            sqlBulk = new SqlBulkCopy(ConfigurationManager.ConnectionStrings["cn"].ConnectionString);
+            sqlBulk.DestinationTableName = "Locations";
+            sqlBulk.ColumnMappings.Add("lat","lat");
+            sqlBulk.ColumnMappings.Add("long","long");            
+            sqlBulk.WriteToServer(dt);
+
+
+            return View();
+
         }
 
         public ActionResult About()
